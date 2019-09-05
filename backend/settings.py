e27a4f0cfd760dba3c1bb9c78809975ded291f13
Patryk Antosz <papoj@wp.pl>
2019-09-05 19:41:56 +0200

    Some changes in settings

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')o5!j@t^$1lier+=_la&p6m%mm-q5=0l)&%l)!^z(pwb1_oziw'
+# SECRET_KEY = ')o5!j@t^$1lier+=_la&p6m%mm-q5=0l)&%l)!^z(pwb1_oziw'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'food',
 ]
 
 MIDDLEWARE = [
