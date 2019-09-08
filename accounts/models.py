e44f6bbf4552d1a09c64e08d39dfd0f10739ffad
Patryk Antosz <papoj@wp.pl>
2019-09-08 11:46:08 +0200

    Added prototype of AppUser model which, for now, is not used

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from food.models import Food
+from enumchoicefield import ChoiceEnum, EnumChoiceField
 
-# Create your models here.
+
+class MealType(ChoiceEnum):
+    BF = "Breakfast"
+    BR = "Brunch"
+    DN = "Dinner"
+    LN = "Lunch"
+    SU = "Supper"
+
+
+class Meal(models.Model):
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    meal_type = EnumChoiceField(enum_class=MealType)
+
+
+class UserFoodHistory(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now())
+
+
+class AppUser(User):
+    food_history = models.ForeignKey(UserFoodHistory, on_delete=models.CASCADE)
