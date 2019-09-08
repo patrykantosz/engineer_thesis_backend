f89a0e9ff38fb8a5ba9d02ec1694fe3d8898b7d2
Patryk Antosz <papoj@wp.pl>
2019-09-08 11:48:10 +0200

    Refactor done by auto-formatter

--- food/models.py
+++ food/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Food(models.Model):
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=255, default="")
     energy_value = models.DecimalField(max_digits=6, decimal_places=3)
     fats = models.DecimalField(max_digits=6, decimal_places=3)
-    saturated_fats = models.DecimalField(max_digits=6, decimal_places=3, default=0)
+    saturated_fats = models.DecimalField(
+        max_digits=6, decimal_places=3, default=0)
     carbohydrates = models.DecimalField(max_digits=6, decimal_places=3)
     sugars = models.DecimalField(max_digits=6, decimal_places=3, default=0)
     proteins = models.DecimalField(max_digits=6, decimal_places=3)
